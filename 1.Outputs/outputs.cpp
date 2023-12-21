0c94ba4daf30adb39ac57f272cd30dbaedb3c664
Abubakr <thefox5000@gmail.com>
2023-12-21 09:46:16 +0300

    :upside_down_face: Use various data types

--- 1.Outputs/outputs.cpp
+++ 1.Outputs/outputs.cpp
@@ -3,11 +3,14 @@ using namespace std;
 
 int main () {
     cout << "********************\n";
-    cout << "Name: Abubakr Ali\n";
+    cout << "Name: \"Abubakr Ali\"\n";
     cout << "Age: 24\n";
     cout << "City: Om-Rowaba\n";
-    cout << "Country: Sudan\n";
+    cout << "Country: \'Sudan\'\n";
     cout << "********************\n";
+    cout << "Dear Sir\\Madan,";
+    cout << "My name is \"Abubakr\", nice to meet you!\n";
+    cout << "Ali\tMona\tKhlid";
     return 1+1;
 }
 
---
+++ 2.Data Types/data.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    string myName = "Abubakr Ali", city = "Om-Rowaba", country = "Sudan";
+    char gender = 'M';
+    int myAge = 23, monthlySalary = 1500;
+    bool married = false;
+    cout << myAge;
+    return 0;
+}
