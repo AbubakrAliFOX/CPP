ac01759287b54f84ec462d67e3624c8436397bfc
Abubakr <thefox5000@gmail.com>
2023-12-22 16:14:59 +0300

    :gear: Add input 1st  homework

---
+++ Homeworks/Input/input-homework.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+int main () {
+    short int A,B;
+    cout << "Please enter 1st Number ";
+    cin >> A;
+
+    cout << "Please enter 2nd Number ";
+    cin >> B;
+
+    cout << "A + B = " << A + B << endl;
+    cout << "A - B = " << A - B << endl;
+    cout << "A * B = " << A * B << endl;
+    cout << "A / B = " << A / B << endl;
+    cout << "A % B = " << A % B << endl;
+    
+    return 0;
+}
