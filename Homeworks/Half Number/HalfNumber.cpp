b6694e6a64895122d9b13a5a898a926a22ee459a
Abubakr <thefox5000@gmail.com>
2023-12-22 16:27:58 +0300

    :notebook: Half number homework

---
+++ Homeworks/Half Number/HalfNumber.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main () {
+    short int Num;
+    cout << "Please enter a number: ";
+    cin >> Num;
+
+    cout << "Half of the number is: " << Num / 2;
+    
+    return 0;
+}
