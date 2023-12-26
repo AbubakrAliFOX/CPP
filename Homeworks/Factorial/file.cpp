736b657034dc9fad684133ded4e23c14d786d6da
Abubakr <thefox5000@gmail.com>
2023-12-26 09:32:40 +0300

    :notebook: Print Factorial Homework

---
+++ Homeworks/Factorial/file.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+using namespace std;
+
+int ReadPositiveNumber(int &N)
+{
+
+    do
+    {
+        cout << "Please Enter a positive number" << endl;
+        cin >> N;
+    } while (N < 0);
+
+    return N;
+}
+
+int CalculateFactorial(int N)
+{
+    int Sum = 1;
+    for (int i = N; i > 0; i--)
+    {
+        Sum = i * Sum;
+    }
+
+    return Sum;
+}
+
+void PrintFactorial(int Result)
+{
+    cout << "Your factorial is: " << endl;
+    cout << Result;
+}
+
+int main()
+{
+    int N;
+    PrintFactorial(CalculateFactorial(ReadPositiveNumber(N)));
+    return 0;
+}
